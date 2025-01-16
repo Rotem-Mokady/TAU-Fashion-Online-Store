39dd5dbe739a874105ea58194c5589cc9cd65403
Rotem-Mokady <rotemmokady@gmail.com>
2025-01-16 17:10:34 +0200

    update cloth DLL without SEX field

--- sql/dll/cloths.sql
+++ sql/dll/cloths.sql
@@ -1,11 +1,10 @@
 CREATE TABLE `cloths` (
   `id` int NOT NULL,
   `name` varchar(45) NOT NULL,
-  `sex` varchar(10) NOT NULL,
   `path` varchar(200) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   `inventory` int NOT NULL,
   `campaign` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`,`name`,`sex`,`path`),
+  PRIMARY KEY (`id`,`name`,`path`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
