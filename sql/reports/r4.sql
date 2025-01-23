25f6908fea5d7e462a966cbe8008899382aa7061
Rotem-Mokady <rotemmokady@gmail.com>
2025-01-23 16:26:22 +0200

    add image addition from manager's page

--- sql/reports/r4.sql
+++
@@ -1,7 +0,0 @@
-SELECT c.id,
-	   c.name,
-       c.price,
-       c.inventory,
-       CASE WHEN c.campaign = 1 THEN 'With Campain' ELSE 'With No Campain' END as campain_flag
-
-FROM taufashion_10.cloths c
